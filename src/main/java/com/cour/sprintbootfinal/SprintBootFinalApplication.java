cf469432160a13aeec31fb8ca1850e2fcc04d597
PouletDesMers <droucook@gmail.com>
2026-04-30 14:10:02 +0200

    first commit

---
+++ src/main/java/com/cour/sprintbootfinal/SprintBootFinalApplication.java
@@ -0,0 +1,13 @@
+package com.cour.sprintbootfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintBootFinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cour/sprintbootfinal/SprintBootFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cour.sprintbootfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintBootFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
